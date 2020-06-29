cad60751d1ad21ba03979ed079b1cfcb7be3d747
sj <sjisyours@gmail.com>
2020-06-29 15:57:07 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cdb69e2abbc84cb83de6.js"
+    "url": "webpack-runtime-17e52f72f6ffd2944b25.js"
   },
   {
     "url": "styles.69c8303230227041b58c.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0937222c3f173ef5194aea917f8d7055"
+    "revision": "75db583642147fcc1da1c3789e53b7ca"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-1af2b3b136460c4ee92c.js"
